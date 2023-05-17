8d3113a8fd98340e440529776f53e163768ce712
Abdullah Nasih Jasir <92930757+njabdullah@users.noreply.github.com>
2023-05-18 05:05:09 +0700

    Update MazeGUI.java

--- src/MazeGUI.java
+++ src/MazeGUI.java
@@ -9,7 +9,6 @@ public class MazeGUI extends JFrame
     public MazeGUI()
     {
     	// Menentukan background, judul, ukuran maze
-    	
         setBackground(Color.BLUE);
         this.maze=new Maze(DefaultMazeSize);
         setTitle("CATOME - Quiz 2");
@@ -17,10 +16,8 @@ public class MazeGUI extends JFrame
         setSize(size,size);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-        
 
         MazePanel mazePanel=new MazePanel(maze);
-
         ButtonPanel westPanel=new ButtonPanel();
         ButtonAction action=new ButtonAction(maze,mazePanel,westPanel);
 
