b0b54842458458b6b7f0e34888322be83a54357a
faizfernanda <101172294+faizfernanda@users.noreply.github.com>
2023-05-18 11:56:23 +0700

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main
+{
+	public static void main(String args[]) throws InterruptedException
+	{	
+		// Menampilkan apa yang sudah dibuat di kelas MazeGUI.java
+    	MazeGUI gui =new MazeGUI();
+        gui.setVisible(true);
+    }
+}
